ce0c5bcaaa4a6b9fb7232da0a1a8e587932c5e51
jcl <15680080051@163.com>
2017-07-07 16:25:10 +0800

    单元测试提交

---
+++ src/main/test/com/huasheng/IdeaJunitTest.java
@@ -0,0 +1,14 @@
+package com.huasheng;
+
+import org.junit.Test;
+
+/**
+ * Created by admin on 2017/7/7.
+ */
+public class IdeaJunitTest {
+
+    @Test
+    public void ideaJunitTest(){
+        System.out.println("使用Idea做单元测试的练习");
+    }
+}
