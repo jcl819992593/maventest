451d17a546d3f6a8485e17f14d92606b674b52dd
jcl <15680080051@163.com>
2017-07-07 16:16:09 +0800

    aaaaa

---
+++ src/test/com/huasheng/JunitTest.java
@@ -0,0 +1,14 @@
+package com.huasheng;
+
+import org.junit.Test;
+
+/**
+ * Created by admin on 2017/7/7.
+ */
+public class JunitTest {
+
+    @Test
+    public void junitTest(){
+        System.out.println("使用Idea做单元测试");
+    }
+}
